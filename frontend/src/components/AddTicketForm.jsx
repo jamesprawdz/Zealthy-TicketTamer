8050bfd69f30a6fd9beffaaa40d640f52dde8cd1
JamesPrawdzik <sebaprawdzik@gmail.com>
2024-07-21 18:05:52 -0400

    Refactor frontend and remove .env from history

---
+++ frontend/src/components/AddTicketForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddTicketForm = () => {
+  return <div>AddTicketForm</div>;
+};
+
+export default AddTicketForm;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "./");
+
+  return {
+    plugins: [react()],
+    base: "/",
+    css: {
+      preprocessorOptions: {
+        css: {
+          charset: false,
+        },
+      },
+    },
+    build: {
+      outDir: "dist",
+    },
+    server: {
+      port: 5173,
+    },
+    envDir: "./",
+    envPrefix: "VITE_",
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(mode),
+      "process.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+    },
+  };
+});
